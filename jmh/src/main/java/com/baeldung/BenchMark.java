6553214aded868d9c8ad847a938ed2d6d4926ebf
Ellen Spertus <spertus@mills.edu>
2020-11-30 17:17:42 -0800

    Reduce warmpu and measurement times

--- jmh/src/main/java/com/baeldung/BenchMark.java
+++ jmh/src/main/java/com/baeldung/BenchMark.java
@@ -1,11 +1,8 @@
 package com.baeldung;
 
-import com.google.common.hash.Hasher;
-import com.google.common.hash.Hashing;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
-import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 public class BenchMark {
@@ -15,37 +12,10 @@ public class BenchMark {
         public int x = 8;
     }
 
-    @State(Scope.Benchmark)
-    public static class ExecutionPlan {
-
-        @Param({ "100", "200", "300", "500", "1000" })
-        public int iterations;
-
-        public Hasher murmur3;
-
-        public String password = "4v3rys3kur3p455w0rd";
-
-        @Setup(Level.Invocation)
-        public void setUp() {
-            murmur3 = Hashing.murmur3_128().newHasher();
-        }
-    }
-
-    @Fork(value = 1, warmups = 1)
-    @Benchmark
-    @BenchmarkMode(Mode.Throughput)
-    @Warmup(iterations = 5)
-    public void benchMurmur3_128(ExecutionPlan plan) {
-
-        for (int i = plan.iterations; i > 0; i--) {
-            plan.murmur3.putString(plan.password, Charset.defaultCharset());
-        }
-
-        plan.murmur3.hash();
-    }
-
     @Benchmark
     @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.Throughput)
     public void init() {
         // Do nothing
@@ -53,13 +23,17 @@ public class BenchMark {
 
     @Benchmark
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public void doNothing() {
-
+        // Do nothing
     }
 
     @Benchmark
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public void objectCreation() {
         new Object();
@@ -67,6 +41,8 @@ public class BenchMark {
 
     @Benchmark
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public Object pillarsOfCreation() {
         return new Object();
@@ -74,12 +50,17 @@ public class BenchMark {
 
     @Benchmark
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public void blackHole(Blackhole blackhole) {
         blackhole.consume(new Object());
     }
 
     @Benchmark
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @BenchmarkMode(Mode.AverageTime)
     public double foldedLog() {
         int x = 8;
 
@@ -87,6 +68,9 @@ public class BenchMark {
     }
 
     @Benchmark
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+    @BenchmarkMode(Mode.AverageTime)
     public double log(Log input) {
         return Math.log(input.x);
     }
